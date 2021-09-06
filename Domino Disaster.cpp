9a878be8ceac634bd20d8e318b9bd79d94c1dc1d
RHRumon <85177445+RHRumon@users.noreply.github.com>
2021-09-06 20:06:38 +0600

    Add files via upload

--- Domino Disaster.cpp
+++ Domino Disaster.cpp
@@ -11,22 +11,24 @@ int main()
         string s;
         cin>>s;
 
-        int i,temp=0;
-        for(i=0;i<n;i++)
+        int i;
+        for(i=0;i<s.size();i++)
         {
-            if(s[i]=='D')
-            {
-                cout<<"U";
-            }
-            else if(s[i]=='U')
-            {
-                cout<<"D";
-            }
-            else if(s[i]!='U' || s[i]!='D')
+            if(s[i]=='L' || s[i]=='R')
             {
                 cout<<s[i];
             }
+            else if(s[i]=='U')
+            {
+                cout<<'D';
+            }
+            else
+            {
+                cout<<'U';
+            }
+
         }
+
     }
     return 0;
 }
